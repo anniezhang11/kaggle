4d9ac6186b1363a60dc70df57a79e51bd039baea
Annie Zhang <zz229@cornell.edu>
2018-12-03 11:55:23 -0500

    python file

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # kaggle
 
 important column names:
-id, text, favoriteCount, created, retweetCount
+id, text, favoriteCount, created, retweetCount, label
+
+label is 1 for android and -1 for iphone
---
+++ trump.py
@@ -0,0 +1,12 @@
+import re
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import string
+import nltk
+
+train  = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+
+train.head()
