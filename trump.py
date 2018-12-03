5940da27526ab2944113ed3bcf05bac6018ba311
Annie Zhang <zz229@cornell.edu>
2018-12-03 14:14:50 -0500

    whoa word cloud cool

--- trump.py
+++ trump.py
@@ -13,24 +13,23 @@ test = pd.read_csv("test.csv")
 
 # clean up data
 combi = train.append(test, ignore_index=True)
-combi["tidy_tweet"] = np.vectorize(combi["text"])
+combi["tidy_tweet"] = np.array(combi["text"])
 combi["tidy_tweet"] = combi["tidy_tweet"].str.replace("[^a-zA-Z#]", " ")
-combi_new = combi[combi["tidy_tweet"].notnull()]
 
 # tokenize
-tokenized_tweet = combi_new["tidy_tweet"].apply(lambda x: x.split())
+tokenized_tweet = combi["tidy_tweet"].apply(lambda x: x.split())
 
 # stem the words
 stemmer = PorterStemmer()
 tokenized_tweet = tokenized_tweet.apply(
     lambda x: [stemmer.stem(i) for i in x]
-)  # stemming
+)  
 
+# stemming
 for i in range(len(tokenized_tweet)):
     tokenized_tweet[i] = " ".join(tokenized_tweet[i])
 
 combi["tidy_tweet"] = tokenized_tweet
-
 all_words = " ".join([text for text in combi["tidy_tweet"]])
 
 wordcloud = WordCloud(
