843e8231e2c89a6e6c51b6b13d3005f6f93f02c7
Aaron Bu <acb256@cornell.edu>
2018-12-06 15:36:24 -0500

    neat

--- random_forest.py
+++ random_forest.py
@@ -13,14 +13,15 @@ from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfTransformer
 
+
 def compute_sentiments(X, data, sentiment_analyzer):
-    num_data_points,_ = X.shape
+    num_data_points, _ = X.shape
 
     sentiment_scores = data.apply(lambda x: sentiment_analyzer.polarity_scores(x))
     neg_sentiment_scores = np.zeros(num_data_points)
     neu_sentiment_scores = np.zeros(num_data_points)
     pos_sentiment_scores = np.zeros(num_data_points)
-    for idx,score in enumerate(sentiment_scores):
+    for idx, score in enumerate(sentiment_scores):
         neg_sentiment_scores[idx] = score["neg"]
         neu_sentiment_scores[idx] = score["neu"]
         pos_sentiment_scores[idx] = score["pos"]
@@ -28,28 +29,37 @@ def compute_sentiments(X, data, sentiment_analyzer):
     neu_sentiment_scores = neu_sentiment_scores.reshape(num_data_points, -1)
     pos_sentiment_scores = pos_sentiment_scores.reshape(num_data_points, -1)
 
-    X = hstack((X,neg_sentiment_scores,neu_sentiment_scores,pos_sentiment_scores))
+    X = hstack((X, neg_sentiment_scores, neu_sentiment_scores, pos_sentiment_scores))
     return X
+
 
 split_boundary = 800
 
 # xTr, yTr = loadData("train.csv", True)
 
+
 def convert_to_seconds(time):
     arr = time.split(":")
-    return int(arr[0])*360 + int(arr[1])*60
-data = pd.read_csv("train.csv")
-testdata = data[split_boundary:]
-data = data[:split_boundary]
+    return int(arr[0]) * 360 + int(arr[1]) * 60
 
-vectorizer = CountVectorizer(stop_words='english', min_df=0.02)
+
+data = pd.read_csv("train.csv")
+
+testdata = pd.read_csv("test.csv")
+
+# testdata = data[split_boundary:]
+# data = data[:split_boundary]
+# yTe = testdata["label"]
+
+
+vectorizer = CountVectorizer(stop_words="english", min_df=0.02)
 xTr = vectorizer.fit_transform(data["text"])
 xTe = vectorizer.transform(testdata["text"])
 yTr = data["label"]
-yTe = testdata["label"]
 
-nTr,_ = xTr.shape
-nTe,_ = xTe.shape
+
+nTr, _ = xTr.shape
+nTe, _ = xTe.shape
 
 tfidf_transformer = TfidfTransformer()
 xTr = tfidf_transformer.fit_transform(xTr)
@@ -75,9 +85,9 @@ xTe = tfidf_transformer.fit_transform(xTe)
 # pos_sentiment_scores = pos_sentiment_scores.reshape(n, -1)
 
 # xTr = hstack((xTr, neg_sentiment_scores, pos_sentiment_scores))
-timesTr = np.array([i.split(' ')[1] for i in data["created"]]).reshape(nTr, -1)
+timesTr = np.array([i.split(" ")[1] for i in data["created"]]).reshape(nTr, -1)
 secondsTr = np.zeros(nTr)
-timesTe = np.array([i.split(' ')[1] for i in testdata["created"]]).reshape(nTe, -1)
+timesTe = np.array([i.split(" ")[1] for i in testdata["created"]]).reshape(nTe, -1)
 secondsTe = np.zeros(nTe)
 for i in range(nTr):
     secondsTr[i] = convert_to_seconds(timesTr[i][0])
@@ -115,17 +125,10 @@ xTe = hstack(
 )
 
 clf = RandomForestClassifier(n_estimators=20).fit(xTr, yTr)
-print(clf.score(xTe, yTe))
-# scores = cross_val_score(clf, xTr, yTr, cv=20)
-# print(scores)
-# print(scores.mean())
+# print(clf.score(xTe, yTe))
 
-# scores = cross_val_score(clf, xTr, yTr, cv=5)
-# print(scores)
-# print(scores.mean())
-
-# scores = cross_val_score(clf, xTr, yTr, cv=5)
-
-
-# print(clf.score(xTr, yTr))
+predictions = clf.predict(xTe)
+submission = pd.DataFrame()
+submission["Label"] = predictions
+submission.to_csv("submission.csv")
 
