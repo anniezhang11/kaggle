c528b1ad77b64dbe3b0f4410c37fdc38cf4c92fc
Harrison Unruh <harrison.unruh@gmail.com>
2018-12-06 14:53:40 -0500

    Modified xTr and xTe

--- random_forest.py
+++ random_forest.py
@@ -11,37 +11,49 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 
+def compute_sentiments(X, data, sentiment_analyzer):
+    num_data_points,_ = X.shape
+
+    sentiment_scores = data.apply(lambda x: sentiment_analyzer.polarity_scores(x))
+    neg_sentiment_scores = np.zeros(num_data_points)
+    neu_sentiment_scores = np.zeros(num_data_points)
+    pos_sentiment_scores = np.zeros(num_data_points)
+    for idx,score in enumerate(sentiment_scores):
+        neg_sentiment_scores[idx] = score["neg"]
+        neu_sentiment_scores[idx] = score["neu"]
+        pos_sentiment_scores[idx] = score["pos"]
+    neg_sentiment_scores = neg_sentiment_scores.reshape(num_data_points, -1)
+    neu_sentiment_scores = neu_sentiment_scores.reshape(num_data_points, -1)
+    pos_sentiment_scores = pos_sentiment_scores.reshape(num_data_points, -1)
+
+    X = hstack((X,neg_sentiment_scores,neu_sentiment_scores,pos_sentiment_scores))
+    return X
+
+split_boundary = 800
+
 # xTr, yTr = loadData("train.csv", True)
 data = pd.read_csv("train.csv")
 vectorizer = CountVectorizer(min_df=0.05)
-xTr = vectorizer.fit_transform(data["text"])
+X = vectorizer.fit_transform(data["text"])
+xTr, xTe = X[:split_boundary],X[split_boundary:]
+yTr, yTe = data["label"][:split_boundary],data["label"][split_boundary:]
+n,_ = xTr.shape
 
 # xTe = vectorizer.transform(testdata["text"])
 # print(xTe.shape)
-yTr = data["label"]
-n,_ = xTr.shape
 
 data["tidy_tweet"] = np.array(data["text"])
 data["tidy_tweet"] = data["tidy_tweet"].str.lower().replace("[^a-zA-Z#]", " ")
 
 sentiment_analyzer = SentimentIntensityAnalyzer()
-sentiment_scores = data["tidy_tweet"].apply(lambda x: sentiment_analyzer.polarity_scores(x))
-neg_sentiment_scores = np.zeros(n)
-neu_sentiment_scores = np.zeros(n)
-pos_sentiment_scores = np.zeros(n)
-for idx,score in enumerate(sentiment_scores):
-    neg_sentiment_scores[idx] = score["neg"]
-    neu_sentiment_scores[idx] = score["neu"]
-    pos_sentiment_scores[idx] = score["pos"]
-neg_sentiment_scores = neg_sentiment_scores.reshape(n, -1)
-neu_sentiment_scores = neu_sentiment_scores.reshape(n, -1)
-pos_sentiment_scores = pos_sentiment_scores.reshape(n, -1)
-xTr = hstack((xTr,neg_sentiment_scores,pos_sentiment_scores))
+xTr = compute_sentiments(xTr, data["tidy_tweet"][:split_boundary], sentiment_analyzer)
+xTe = compute_sentiments(xTe, data["tidy_tweet"][split_boundary:], sentiment_analyzer)
 
-clf = RandomForestClassifier(n_estimators=15)
-scores = cross_val_score(clf, xTr, yTr, cv=5)
-print(scores)
-print(scores.mean())
+clf = RandomForestClassifier(n_estimators=15).fit(xTr, yTr)
+print(clf.score(xTe, yTe))
+# scores = cross_val_score(clf, xTr, yTr, cv=20)
+# print(scores)
+# print(scores.mean())
 
 # scores = cross_val_score(clf, xTr, yTr, cv=5)
 # clf = clf.fit(xTr, yTr)
